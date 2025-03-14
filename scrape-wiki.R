210c72146e5ad0519e36a4e842c761d951ed6229
Tom Saunders <tom.saunders@auckland.ac.nz>
2025-03-14 16:24:51 +1300

    Update wikipedia example script, update slides

---
+++ scrape-wiki.R
@@ -0,0 +1,51 @@
+# Load packages
+
+library(rvest)
+library(dplyr)
+library(readr)
+
+# Read in electricity table from wikipedia
+
+html_electricity <- read_html("https://en.wikipedia.org/wiki/List_of_countries_by_electricity_consumption")
+
+# Grab the table of interest, rename columns,  remove 'world' row and 'year' column
+
+electricity <-
+  html_electricity |> 
+  html_element("table.wikitable") |> 
+  html_table() |> 
+  rename(
+    location = "Location",
+    consumption_twh = "Consumption(TWh)",
+    per_capita_mwh = "Per capita(MWh)",
+  ) |> 
+  filter(location != "World") |> 
+  select(-Year)
+
+# Convert consumption to numeric
+
+electricity$consumption_twh <-
+  parse_number(electricity$consumption_twh)
+
+# Read in another table - incarceration rates from Wikipedia
+
+html_incarceration <- read_html("https://en.wikipedia.org/wiki/List_of_countries_by_incarceration_rate")
+
+# Convert to a table, drop 'Number', and rename column
+
+incarceration <-
+  html_incarceration |> 
+  html_element("table.wikitable") |> 
+  html_table() |> 
+  select(-Number) |> 
+  rename(incarceration_rate = Rates)
+
+# Join the two tables based on country name
+
+final_table <-
+  left_join(x = electricity, 
+            y = incarceration,
+            by = join_by(location == Location))
+
+
+
